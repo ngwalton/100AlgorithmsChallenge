363575856baac76a63ec758cd0a6d17aafee79f8
Nicholas Walton <ngwalton@gmail.com>
2021-07-29 17:39:28 -0500

    Add code, turn on and fix tests for addBorder

--- addBorder/addBorder.spec.ts
+++ addBorder/addBorder.spec.ts
@@ -1,9 +1,9 @@
 import { addBorder } from './addBorder';
 
-xdescribe(addBorder.name, () => {
+describe(addBorder.name, () => {
     it('Test 1', () => {
         // arrange
-        const data = ['abc, ded'];
+        const data = ['abc', 'ded'];
 
         // act
         const response = addBorder(data);
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,9 @@
 export function addBorder(picture: string[]): string[] {
+    const char: string = "*";
+    let result: string[] = picture.map(str => char + str + char);
+    const topBottom: string = char.repeat(result[0].length);
 
+    return [topBottom, ...result, topBottom];
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
