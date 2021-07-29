cd174d5e9e263470ae55a913af4a01f51d9e97d8
Nicholas Walton <ngwalton@gmail.com>
2021-07-29 16:28:27 -0500

    Add code, enable tests absoluteValuesSumMinization

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,11 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+    const isEven: boolean = a.length % 2 === 0;
+    const medianIndx: number = Math.floor(a.length / 2);
+
+    return isEven ? a[medianIndx - 1] : a[medianIndx];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
